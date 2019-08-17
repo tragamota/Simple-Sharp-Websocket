1251f0c6d3d3cfbb160c4b3341b84f4e7624e41d
Ian van de Poll <ian.vd.poll@gmail.com>
2019-08-17 13:17:05 +0200

    Update UML Diagram

--- Simple Sharp Websocket/SocketClient.cs
+++ Simple Sharp Websocket/SocketClient.cs
@@ -5,17 +5,27 @@ namespace SimpleSharp.Websocket
 {
     internal class SocketClient
     {
-        private TcpClient basesocket;
+        private TcpClient baseSocket;
         private Stream baseStream;
         private SocketReader reader;
         private SocketWriter writer;
+        
         public SocketClient(TcpClient baseSocket, Stream baseStream)
         {
-            this.basesocket = baseSocket;
+            this.baseSocket = baseSocket;
             this.baseStream = baseStream;
+            this.reader = new SocketReader(baseStream);
+            this.writer = new SocketWriter(baseStream);
         }
 
 
+
+        public void Close() {
+            baseSocket.Close();
+            baseStream.Close();
+            reader.Close();
+            writer.Close();
+        }
 
     }
 }
--- Simple Sharp Websocket/SocketWriter.cs
+++ Simple Sharp Websocket/SocketWriter.cs
@@ -1,6 +1,82 @@
+using System;
+using System.IO;
+using System.Text;
+
 namespace SimpleSharp.Websocket
 {
     internal class SocketWriter
     {
+        public Stream BaseStream { get; private set; }
+
+        public Encoding StreamEncoding { get; private set; }
+
+        public SocketWriter(Stream baseStream) : this(baseStream, Encoding.UTF8) { }
+
+        public SocketWriter(Stream baseStream, Encoding encoding)
+        {
+            this.BaseStream = baseStream;
+            this.StreamEncoding = encoding;
+        }
+
+        public void WriteBytes(byte[] buffer)
+        {
+            if (buffer != null)
+            {
+                try
+                {
+                    BaseStream.Write(buffer, 0, buffer.Length);
+                }
+                catch (SystemException)
+                {
+                    //stream error
+                }
+            }
+        }
+
+        public async void WriteBytesAsync(byte[] buffer) {
+            if(buffer != null) {
+                try {
+                    await BaseStream.WriteAsync(buffer, 0, buffer.Length);
+                }
+                catch(SystemException) {
+                    //stream error 
+                }
+            }
+        }
+
+        public void WriteString(string sendString) {
+            if(!string.IsNullOrEmpty(sendString)) {
+                byte[] encodedBuffer = EncodeString(sendString);
+                
+                try {
+                    BaseStream.Write(encodedBuffer, 0, encodedBuffer.Length);
+                }
+                catch(SystemException) {
+                    //stream error
+                }
+            }
+        }
+
+        public void WriteStringAsync(string sendString) {
+            if(!string.IsNullOrEmpty(sendString)) {
+                byte[] encodedBuffer = EncodeString(sendString);
+
+                try{
+                    BaseStream.Write(encodedBuffer, 0, encodedBuffer.Length);
+                }
+                catch(SystemException) {
+                    //stream error
+                }
+            }
+        }
+
+        private byte[] EncodeString(string unencodedString) {
+            return StreamEncoding.GetBytes(unencodedString);
+        }
+
+        public void Close() {
+            BaseStream.Close();
+    
+        }
     }
 }
