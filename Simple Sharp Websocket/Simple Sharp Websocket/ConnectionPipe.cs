bfeb37d7668d5bda7dbe0024defc068bfe3697bd
Ian van de Poll <ian.vd.poll@gmail.com>
2019-09-17 17:09:59 +0200

    Add initial data pipeline

---
+++ Simple Sharp Websocket/Simple Sharp Websocket/ConnectionPipe.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.IO.Pipelines;
+
+namespace Simple_Sharp_Websocket
+{
+    public struct ConnectionPipe
+    {
+        public IDuplexPipe SocketPipe { get; }
+        public IDuplexPipe ApplicationPipe { get; }
+
+        public ConnectionPipe(PipeOptions inputOptions = default, PipeOptions outputOptions = default)
+        {
+            Pipe input = new Pipe(inputOptions);
+            Pipe output = new Pipe(outputOptions);
+            
+            CreatePipelinePair(input, output);
+        }
+
+        private void CreatePipelinePair(Pipe inputPipe, Pipe outputPipe)
+        {
+            TransportPipe SocketPipe =     
+        }
+    }
+}
---
+++ Simple Sharp Websocket/Simple Sharp Websocket/DuplexPipe.cs
@@ -0,0 +1,17 @@
+﻿using System.IO.Pipelines;
+using System.Reflection.Emit;
+
+namespace Simple_Sharp_Websocket
+{
+    public class TransportPipe : IDuplexPipe
+    {
+        public PipeReader Input { get; }
+        public PipeWriter Output { get; }
+        
+        public TransportPipe(PipeReader reader, PipeWriter writer)
+        {
+            this.Input = reader;
+            this.Output = writer;
+        }
+    }
+}
